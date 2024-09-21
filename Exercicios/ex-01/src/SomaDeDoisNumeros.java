cd028be5653e0b3f69bde02147a062f26e2d5c13
Bowie <igowmourra@gmail.com.br>
2024-09-20 21:55:12 -0300

    update

---
+++ Exercicios/ex-01/src/SomaDeDoisNumeros.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SomaDeDoisNumeros {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("\nDigite o primeiro numero inteiro:");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite o segundo numero inteiro:");
+        int numero2 = scanner.nextInt();
+
+        int soma = numero1 + numero2;
+        System.out.println("A soma dos dois números é: " + soma);
+        scanner.close();
+
+    }
+}
