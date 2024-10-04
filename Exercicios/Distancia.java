4768c140528779fb435fb68e2dcdebd3a3bea43d
Bowie <igowmourra@gmail.com.br>
2024-10-04 00:38:59 -0300

    Update

---
+++ Exercicios/Distancia.java
@@ -0,0 +1,31 @@
+
+/* Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+
+Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
+
+Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
+
+Entrada
+O arquivo de entrada contém um número inteiro.
+
+Saída
+Imprima o tempo necessário seguido da mensagem "minutos".*/
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    Scanner leia = new Scanner(System.in);
+
+        int X , Distancia;
+
+        X = leia.nextInt();
+
+        Distancia = X * 2;
+
+        System.out.printf("%d minutos\n",Distancia);
+        
+    }
+}
---
+++ Exercicios/Gasto_de_Combustivel.java
@@ -0,0 +1,30 @@
+
+/* Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
+
+Entrada
+O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
+
+Saída
+Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal*/
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    Scanner leia = new Scanner(System.in);
+
+        int A , B;
+        double Tempo;
+
+        A = leia.nextInt();
+        B = leia.nextInt();
+
+        Tempo = A * B;
+        Tempo = Tempo / 12;
+
+        System.out.printf("%.3f\n",Tempo);
+        
+    }
+}
