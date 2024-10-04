50091d22745df7abf652186b64bf910e3de33e6e
Bowie <igowmourra@gmail.com.br>
2024-10-03 22:54:53 -0300

    Create Consumo.java

---
+++ Exercicios/Consumo.java
@@ -0,0 +1,29 @@
+
+/*Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+Entrada
+O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km), e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+Saída
+Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l". */
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    Scanner leia = new Scanner(System.in);
+
+        int X ;
+        double Y , Distancia ;
+
+        X = leia.nextInt();
+        Y = leia.nextDouble();
+
+        Distancia = X / Y;
+
+        System.out.printf("%.3f km/l\n",Distancia);
+        
+    }
+}
